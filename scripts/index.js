7c767cdfaf1b1010ce19f395076bf4eb7a9cca23
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-05 14:25:28 +0200

    hw_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,27 @@
 'use strict'
 
+let a = Number(prompt('Write first number'))
+
+
+let b = Number(prompt('Write second number'))
+
+function plus(){
+    return  a + b;
+}
+
+function minus(){
+    return a - b;
+}
+
+function multiply(){
+    return a * b;
+}
+
+function divide(){
+    return a / b;
+}
+
+console.log('Result:', a, '+', b, '=', plus())
+console.log('Result:', a, '-', b, '=', minus())
+console.log('Result:', a, '*', b, '=', multiply())
+console.log('Result:', a, '/', b, '=', divide())
