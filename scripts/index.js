0f7ca1814fbbc97c6dac004388ece328933cee2d
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-05 14:32:56 +0200

    tasks

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,26 @@
-'use strict'
+"use strict";
 
+// Task 1
+
+let admin;
+let name = 'John';
+
+admin = name;
+
+alert(admin)
+
+
+// Task 2
+
+
+let ourPlanetName = 'Earth';
+
+let currentUserName = 'Sam'
+
+
+// Task 3
+
+
+let stringPlusNumb = '10'+ 10
+
+console.log(typeof stringPlusNumb)
