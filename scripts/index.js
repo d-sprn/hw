1f54eca9cd1c1c8f170c645862f8ae12ee282d67
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-05 14:28:55 +0200

    another code for calc

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,29 @@
 'use strict'
 
+let a = Number(prompt('Write first number'))
+
+let b = Number(prompt('Write second number'))
+
+let operator = prompt('Write operator: + ; - ; * ; / ;' )
+
+function calc(){
+    switch (operator){
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if(b !== 0){
+                return a / b;
+            } else{
+                return "Write correct second number"
+            }
+
+    }
+}
+
+
+console.log('Result:', a, operator, b, '=', calc())
+
