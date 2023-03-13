413e5462bd9f2c8defedaf79712a21f8868df68c
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-13 16:08:54 +0200

    hw_9

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// Task #1
+
+const a = Number(prompt('Write first number'))
+
+const b = Number(prompt('Write second number'))
+
+function equalityOne(){
+    return alert(a === b);
+}
+
+equalityOne()
+
+
+
+// Task #2
+
+function equalityTwo(){
+    return alert( (a + b) === 10);
+}
+
+equalityTwo()
+
+
+
+
+// Task 3
+
+const c = Number(prompt('Write number'))
+
+
+function negative(){
+    return alert(c < 0)
+}
+
+negative()
