afcc6ce69838ac3f9e80dbe61dba024551d0281a
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-13 21:47:51 +0200

    In progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,48 @@
 "use strict";
 
+const age = prompt('Write your age');
+
+const city = prompt('Write your city');
+
+// const sport = prompt('Write your favourite sport');
+let sport = null;
+
+
+let userCountry = null;
+
+let userSport = null;
+
+
+if(age === true && city === true){
+    sport = prompt('Write your favourite sport');
+}
+
+switch (city){
+    case 'Kiev':
+        userCountry = 'Ukraine';
+        break;
+    case 'moscow':
+        userCountry = 'russia';
+        break
+    case 'minsk':
+        userCountry = 'belorusia';
+        break
+}
+
+switch (sport){
+    case 'football':
+        userSport = 'Ronaldo';
+        break;
+    case 'tennis':
+        userSport = 'Federer';
+        break;
+    case 'hockey':
+        userSport = 'Gretzky';
+        break;
+}
+
+// let summary = ('Your age: ', age, 'You live in capital of: ', userCountry, 'You want to be like: ', userSport)
+
+alert(('Your age: ', age, 'You live in capital of: ', userCountry, 'You want to be like: ', userSport))
+
+// alert(summary)
