c98f9d5aa0a63a41ac520d7a7cbc9c86195343e2
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-13 17:14:28 +0200

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,39 @@
 "use strict";
 
-const value = Number(prompt('Write number'));
+// Task #1
 
+
+
+const value = Number(prompt('Write number between 0 and 10'));
+console.log(value)
+
+
+if(value > 0 && !isNaN(value) && value < 10){
+    alert("Right");
+} else{
+    alert('try again');
+}
+
+
+
+// Task #2
+
+
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+
+switch (numOrStr){
+    case null:
+        console.log('Canceled');
+        break;
+    case '':
+        console.log('Empty String');
+        break;
+    case NaN:
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+}
