3800483b437b70f2b87f0df96f6b0960b4c95503
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-14 21:12:26 +0200

    In progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,25 @@
 "use strict";
 
+// Task #1
+
+
+let x = 10, y = 7;
+
+x > y ? alert('x more then y'):alert('x less than y')
+
+
+// Task #2
+
+
+let num = +prompt('Enter number');
+let checkNum = num % 2;
+if ((checkNum = 0)) {
+  alert(`Number ${num} is even`);
+} else {
+  alert(`Number ${num} is odd`);
+}
+
+
+// Task #3
+
+
