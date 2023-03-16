34c431eb95000f1a387d374f34b46178ceaf8093
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-16 12:09:41 +0200

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
 "use strict";
 
+// Task #1
+
+const x = 4;
+const y = 9;
+
+const radius = 10;
+
+let circlePoint = (radius > (Math.sqrt((x**2) + (y**2))))? 'Yes, it\'s true' : 'No, it\'s false';
+
+alert(circlePoint)
