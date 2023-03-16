94b42d5255dd187e2aeed382ed7eb14f4a4b6b06
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-16 11:50:34 +0200

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 // Task #1
 
 
@@ -11,9 +12,10 @@ x > y ? alert('x more then y'):alert('x less than y')
 // Task #2
 
 
-let num = +prompt('Enter number');
-let checkNum = num % 2;
-if ((checkNum = 0)) {
+const num = +prompt('Enter number');
+const checkNum = num % 2;
+
+if (checkNum === 0) {
   alert(`Number ${num} is even`);
 } else {
   alert(`Number ${num} is odd`);
@@ -23,3 +25,66 @@ if ((checkNum = 0)) {
 // Task #3
 
 
+
+const num1= +prompt('Ender negative or positive number');
+const checkNum2 = num1 < 0;
+
+let checkedNum = null;
+
+if(checkNum2 === true){
+  checkedNum = `Number ${num1} is negative!`;
+} else {
+  checkedNum = `Number ${num1} is positive!!`;
+}
+
+
+alert(`${checkedNum}`)
+
+// Task #4
+
+const num2 = +prompt('Enter the three-digit number');
+const numString = String(num2);
+
+let numStringResult = null;
+
+
+
+if(numString.slice(0, 1) > numString.slice(1, 2) && numString.slice(0, 1) > numString.slice(2, 3)){
+  numStringResult = `Largest number is ${numString.slice(0, 1)}!!`;
+}
+else if(numString.slice(1, 2) > numString.slice(0, 1) && numString.slice(1, 2) > numString.slice(2, 3)){
+  numStringResult = `Largest number is ${numString.slice(1, 2)}!!`;
+}
+else if(numString.slice(2, 3) > numString.slice(0, 1) && numString.slice(2, 3) > numString.slice(1, 2)){
+  numStringResult = `Largest number is ${numString.slice(2, 3)}!!`;
+}
+else if(numString.slice(0, 1) === numString.slice(1, 2) && numString.slice(0, 1) === numString.slice(2, 3) && numString.slice(1, 2) === numString.slice(2, 3)) {
+  numStringResult = 'All number is same!!!';
+}
+else{
+  numStringResult = 'Try again :(';
+}
+
+
+alert(`${numStringResult} \n The number of symbols: ${(numString.length)}!!`)
+
+
+// Task #5
+
+
+const a = +prompt('Write first length');
+const b = +prompt('Write second length');
+const c = +prompt('Write third length');
+
+
+let result = null;
+
+
+if(a + b > c && a + c > b && c + b > a){
+  result = 'Triangle done!!';
+} else {
+  result = 'Triangle broke :('
+}
+
+
+alert(`${result}`)
