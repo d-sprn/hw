aa5b6dc8366c18f3873d9dcdb2efb6d2c9f83f63
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-21 19:43:34 +0200

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const arr = [1, 2, 3, -1, -2, -3];
+
+
+function positiveFunc(){
+    const funcArray = [];
+    let resultArray = [];
+
+    if (arr.length === 0) { return console.log( 'Empty Arrow' ) }
+    else if (arr.length > 0) {
+        for (let i = 0 ; i < arr.length; i++){
+            if ( arr[i] >= 0 ){
+                funcArray.push(arr[i]);
+            }
+        }
+    }
+    resultArray = funcArray.length > 0 ? funcArray : null;
+
+    return  console.log(resultArray)
+}
+
+positiveFunc()
+
