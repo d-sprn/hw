4dc2c413e22b5d694996d0e054e6739258c1b234
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-21 20:49:40 +0200

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 "use strict";
 
+function fib(n){
+
+}
 const number = +prompt('Write number.');
 const power = +prompt('Raised to the power of?');
 
-function pow(number = 1, power){
+function pow(number, power = 1){
     if (isNaN(number)|| isNaN(power)){
         return alert('some error');
-    } else { return number ** power; }
+    }
+    return number ** power;
 }
 
 const result = pow(number, power);
