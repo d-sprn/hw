c174b3ab25258f47662bcc6cdda1f2c2da6a7013
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-21 20:24:57 +0200

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,13 @@
 "use strict";
 
-
 function calcBorsh(){
     let oneLtr = 4;
     let potato = oneLtr * 0.075;
-    let price = 13
-    let totalLtr = 48
+    let price = 13;
+    let totalLtr = 48;
     let totalPrice = Math.ceil(totalLtr * potato) * price;
 
-    console.log(totalPrice)
+    console.log(totalPrice);
 }
 
 calcBorsh()
