d103de77858c4ae906788c9f34391fce43a80280
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-22 20:11:23 +0200

    done

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 "use strict";
 
+let userNumber = +prompt('Write number.');
+
+let result = null;
+
+for (let i = 0; i < userNumber; i++){
+    if (userNumber / i === 1){
+        result = false
+    }
+
+}
+
+console.log(result)
