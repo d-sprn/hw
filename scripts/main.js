68ab6c901adef81fc64b8ff242a460c80d29eda3
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-22 11:46:46 +0200

    done

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
 "use strict";
 
+
+
+function fib(n){
+    let firstNumb = 1;
+    let secondNumb = 1;
+    for (let i = 3; i <=n ; i++){
+        let thirdNumber = firstNumb + secondNumb;
+        firstNumb = secondNumb;
+        secondNumb = thirdNumber;
+    }
+    return secondNumb;
+}
+
+alert(fib(3))
