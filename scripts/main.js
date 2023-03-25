05ae8e13b96b558a1a0eb51956a5aeb58f647723
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-25 17:59:57 +0200

    done

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ const checkFunction = (number) => {
     const arr = [];
 
     if (isNaN(number) || !isFinite(number)) return 'It is not number';
-    if (number <= 1) return NaN
 
     for (let i = 2; i <= number; i++){
         if (number % i === 0) { arr.push(i)}
