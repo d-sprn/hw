fdfdca9f5b2828858e283c1ebe5df21d79290452
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-27 10:39:19 +0300

    done

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,14 @@ const arr = [1, 2, 3, -1, -2, -3];
 
 function positiveFunc(arr){
     const funcArray = [];
-    let resultArray = [];
 
-    if (arr.length === 0) { return 'Empty Arrow' }
+    if (arr.length === 0)  return 'Empty Arrow';
     if (arr.length > 0) {
         for (let i = 0 ; i < arr.length; i++){
-        if ( arr[i] >= 0 ) { funcArray.push(arr[i]) }
+        if ( arr[i] >= 0 ) funcArray.push(arr[i])
         }
     }
-
-    resultArray = funcArray.length > 0 ? funcArray : null;
-
-    return  resultArray;
+    return  funcArray.length > 0 ? funcArray : null;
 }
 
 console.log(positiveFunc(arr));
