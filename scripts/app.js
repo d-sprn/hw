8efb55c6ecc3e3c2998ab3fad6c9b04677d2503b
Dima <122263971+dmsprn@users.noreply.github.com>
2023-03-27 12:31:07 +0300

    Done

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,35 @@
 "use strict";
 
+
+
+const powerFunction = (number, power) => {
+    const powerResult = number**power;
+    return alert(`${number} raised to power of ${power} equal ${powerResult}`);
+}
+
+const multiply = (number, power) => {
+    const multiplyResult = number * power;
+    return alert(`${number} multiply ${power} equal ${multiplyResult}`);
+}
+
+const division = (number, power) =>{
+    const divisionResult = number / power;
+    return alert(`${number} division ${power} equal ${divisionResult}`);
+}
+
+const modulo = (number, power) =>{
+    const moduloResult = number % power;
+    return alert(`${number} modulo ${power} equal ${moduloResult}`);
+}
+
+
+
+const mainFunction = (callback) =>{
+    const number = Number(prompt('Write number.'));
+    const power = Number(prompt('Write second number'));
+    if ( number === isNaN(number) || number === isFinite(number) ) return 'Please write number!!';
+
+    return (powerFunction(number, power), multiply(number, power), division(number, power), modulo(number, power))
+}
+
+console.log(mainFunction());
