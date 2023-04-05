16061857c33d15f8e1bb8cb5275be73bc4917d4a
Dima <122263971+dmsprn@users.noreply.github.com>
2023-04-05 21:14:42 +0300

    Done

--- scripts/app.js
+++ scripts/app.js
@@ -2,199 +2,132 @@
 
 // Task #1
 
+const arr1 = ['a', 'b', 'c', 'd'];
 
-const firstArray1 = [1, 2, 3] ;
-const secondArray1 =  [4, 5, 6];
-
-const resultArray1 = [...firstArray1, ...secondArray1];
-
+alert (`${arr1[0] + arr1[1]}  ,  ${arr1[2]+arr1[3]}`)
 
 // Task #2
 
 
-const array2 = [1, 2, 3];
+const arr2 = [2, 5, 3, 9];
 
-array2.reverse()
-
+const arrFunc2 =() =>{
+   let a = arr2[0] * arr2[1];
+    let b = arr2[2] * arr2[3];
+    let result = a + b;
+    return alert(result);
+}
+arrFunc2();
 
 // Task #3
 
+const arr3 =  [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
 
-const array3 = [1, 2, 3];
 
-array3.push(4, 5, 6)
+alert(arr3[1][0]);
+
 
 // Task #4
 
 
-const array4 = [1, 2, 3];
+const object4 =  {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}
 
-array4.unshift(4, 5, 6);
+alert(object4['js'][0])
 
 
 // Task #5
 
 
-const array5 =  ['js', 'css', 'jq'];
+const arr5 = [];
+let symb5 = 'x';
 
-alert(array5[0]);
+for (let i = 0; i < 10; i++){
+    arr5.push(symb5);
+    symb5 += 'x';
+}
 
 
 // Task #6
 
 
-const array6 =  ['js', 'css', 'jq'];
+const arr6 = [];
 
-alert(array6[array5.length-1]);
+let symb6 = 1;
+
+for (let i = 1; i <= 9; i++) {
+    arr6.push(`${i}`.repeat(i));
+}
+
 
 
 // Task #7
 
 
-const array7 = [1, 2, 3, 4, 5];
-
-const resultArray7 = array7.splice(0,3);
+function arrayFill(value, count) {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+        arr.push(value);
+    }
+    return arr;
+}
 
 
 // Task #8
 
 
-const array8 = [1, 2, 3, 4, 5];
+const arr8 = [2, 4, 1, 3, 5, 6];
 
-const resultArray8 = array8.splice(3, array8.length);
+let sum8 = 0;
+let count8 = 0;
+
+for (let i = 0; i < arr8.length; i++) {
+    sum8 += arr8[i];
+    count8++;
+    if (sum8 > 10) {
+        break;
+    }
+}
 
 
 // Task #9
 
 
-const array9 = [1, 2, 3, 4, 5];
+const arr9 = [1, 2, 3, 4, 5];
 
-array9.splice(1,2)
+for (let i = 0; i < arr9.length / 2; i++) {
+    const num = arr9[i];
+    arr9[i] = arr9[arr9.length - 1 - i];
+    arr9[arr9.length - 1 - i] = num;
+}
 
 
 // Task #10
 
 
-const array10 = [1, 2, 3, 4, 5];
+const arr10 = [[9, 4, 7], [1, 3], [6]];
+let sum10 = 0;
 
-const resultArray10 = array10.splice(1,3);
+for (let i = 0; i < arr10.length; i++) {
+    for (let j = 0; j < arr10[i].length; j++) {
+        sum10 += arr10[i][j];
+    }
+}
 
 
 
 // Task #11
 
 
-const array11 = [1, 2, 3, 4, 5];
+const arr11 = [[[2, 2], [3, 3]], [[4, 4], [5, 5]]];
+let sum11 = 0;
 
-array11.splice(4, 0, 'a', 'b', 'c');
-
-
-
-// Task #12
-
-
-const array12 = [1, 2, 3, 4, 5];
-
-array12.splice(1, 0, 'a', 'b');
-array12.splice(array12.length-1,0, 'c');
-array12.splice(array12.length, 0, 'e');
-
-
-// Task #13
-
-
-const array13 = [3, 4, 1, 2, 7];
-
-array13.sort();
-
-
-// Task #14
-
-
-const array14 = [5, 6, 7, 8, 9];
-let sum14 = null;
-
-for(let i = 0; i < array14.length; i++){
-    sum14 += array14[i];
-}
-
-
-// Task #15
-
-
-let array15 = [5, 6, 7, 8, 9];
-
-const arrFunc15 = () => {
-
-    let sum15 = [];
-
-    for (let i = 0; i < array15.length; i++){
-        sum15.splice(i, 0, Math.pow(array15[i], 2));
+for (let i = 0; i < arr11.length; i++) {
+    for (let j = 0; j < arr11[i].length; j++) {
+        for (let k = 0; k < arr11[i][j].length; k++) {
+            sum11 += arr11[i][j][k];
+        }
     }
-    return array15 = sum15
 }
 
-arrFunc15()
 
-
-// Task #16
-
-const arrFunc16 = (value) => {
-    return value < 0;
-}
-let array16 = [1,-3, 5, 6,-7, 8, 9,-11].filter(arrFunc16);
-
-
-// Task #17
-
-const arrFunc17 = (value) => {
-    return value % 2 === 0;
-}
-
-let array17 = [1,-3, 5, 6,-7, 8, 9,-11].filter(arrFunc17);
-
-
-// Task #18
-
-
-const arrFunc18 = (word) => {
-    return word.length > 5
-}
-
-const array18 = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'].filter(arrFunc18)
-
-
-
-// Task #19
-
-
-const arrFunc19 = (array) => {
-    return Array.isArray(array)
-}
-
-const array19 = [1, 2, [3, 4], 5, [6, 7]].filter(arrFunc19)
-
-
-// Task #20
-
-
-const arrFunc20 = (num) => {
-    return num < 0;
-}
-
-const array20 =  [5,-3, 6,-5, 0,-7, 8, 9];
-const result20 = array20.filter(arrFunc20)
-
-console.log(result20.length)
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(sum11)
