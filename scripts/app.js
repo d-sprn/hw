218467bee48a84d9265725029147390e7479b03f
Dima <122263971+dmsprn@users.noreply.github.com>
2023-04-25 18:29:25 +0300

    Done

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,29 @@
-'use strict'
+'use strict';
 
 void (function () {
-  const calculator = {
-
-    first: 0,
-    second: 0,
-
-    read(){
-      this.first = +prompt('Write first number');
-      this.second = +prompt('write second number');
-      if (!isNaN(this.first) && !isNaN(this.second)){
-        return  { first: this.first, second: this.second}
-      } else { return console.log('Write number') }
-    },
-
-    sum(){
-      return this.first + this.second;
-    },
-
-    mul(){
-      return this.first * this.second
+  function bind(fn, context, ...rest) {
+    return function(...args) {
+      const id = Date.now().toString();
+      context[id] = fn;
+      const res = context[id](...rest, ...args);
+      delete context[id];
+      return res;
     }
   }
 
+  const user = {
+    firstName: '',
+    lastName: '',
+    name() {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
 
-  calculator.read();
-  alert( calculator.sum() );
-  alert( calculator.mul() );
-})()
+  function fullName(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    return this.name();
+  }
 
 
-
-
-
+})();
