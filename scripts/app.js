9cef33aa25ef6a1dec221d0ac72ff46f53645818
Dima <122263971+dmsprn@users.noreply.github.com>
2023-04-27 14:47:09 +0300

    Done

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,20 @@
 // Task #1
 
 void (function () {
-  const func1 = (n) => {
+  const getFactorial = (n) => {
     if (n < 1) return 1;
-    return n * func1(n - 1);
+    return n * getFactorial(n - 1);
   };
 })();
 
 // Task #2
 
 void (function () {
-  const func2 = (n, pow) => {
+  const powFunc = (n, pow) => {
     if (pow === 1) {
       return n;
     }
-    return n * func2(n, pow - 1);
+    return n * powFunc(n, pow - 1);
   };
 
   func2(2, 5);
@@ -23,8 +23,8 @@ void (function () {
 // Task #3
 
 void (function() {
-  const func3 = (a, b) => {
+  const recursionAdd = (a, b) => {
     if (b === 0) return a;
-    return func3(a + 1, b - 1);
+    return recursionAdd(a + 1, b - 1);
   };
 })
