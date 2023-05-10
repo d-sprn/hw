d3f7b1ead154a8ee73acd7092c1a93162b01bb0a
Dima <122263971+dmsprn@users.noreply.github.com>
2023-05-10 10:54:14 +0300

    Done

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,24 @@
-'use strict';
+"use strict";
 
-const generateList = (array) => {
-  const ul = document.createElement('ul');
-  for (let i = 0; i < array.length; i++) {
-    const li = document.createElement('li');
-    if (Array.isArray(array[i])) {
-      li.appendChild(generateList(array[i]));
-    } else {
-      li.innerHTML = array[i];
+void (function () {
+  function createTable() {
+    const table = document.createElement("table");
+    let count = 1;
+
+    for (let i = 0; i < 10; i++) {
+      const tr = document.createElement("tr");
+      for (let j = 0; j < 10; j++) {
+        const td = document.createElement("td");
+        td.textContent = count;
+        tr.appendChild(td);
+        count++;
+      }
+      table.appendChild(tr);
     }
-    ul.appendChild(li);
+
+    return table;
   }
-  return ul;
-}
 
-
-const myArray = [1, 2, [1.1, 1.2, 1.3], 3];
-const myList = generateList(myArray);
-document.body.appendChild(myList);
+  const table = createTable();
+  document.body.appendChild(table);
+})();
